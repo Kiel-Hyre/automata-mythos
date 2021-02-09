b868c09be2b4b39512c4b1d0be457797e504ee58
Espeys <contreras.jerico720@gmail.com>
2021-02-09 23:49:47 +0800

    create lexical table

--- src/App.vue
+++ src/App.vue
@@ -9,40 +9,67 @@
             width="300"
           />
         </div>
-        <div class="mt-10 mb-5">
-          <span
-            >Press <kbd dark>alt + ctrl + s</kbd> to save,
-            <kbd dark>alt + ctrl + r</kbd> to run</span
-          >
-        </div>
-        <v-sheet color="#2d2d2d" class="py-3">
-          <prism-editor
-            v-model="code"
-            cols="5"
-            :highlight="highlight"
-            class="my-editor px-3 "
-            line-numbers
-            @keyup.alt.83="saveChanges"
-            @keyup.alt.82="execute"
-            @keydown="smartTyper"
-          ></prism-editor>
-        </v-sheet>
+        <div>
+          <div>
+            <div class="mt-10 mb-5">
+              <span
+                >Press <kbd dark>alt + ctrl + s</kbd> to save,
+                <kbd dark>alt + ctrl + r</kbd> to run</span
+              >
+            </div>
+            <v-sheet color="#2d2d2d" class="py-3">
+              <prism-editor
+                v-model="code"
+                cols="5"
+                :highlight="highlight"
+                class="my-editor px-3"
+                line-numbers
+                @keyup.alt.83="saveChanges"
+                @keyup.alt.82="execute"
+                @keydown="smartTyper"
+              ></prism-editor>
+            </v-sheet>
 
-        <v-btn @click="execute" class="success text-none mt-10" large>
-          <v-icon
-            v-text="'fa-cog'"
-            size="20"
-            :class="{ rotating: loading }"
-            left
-          />
-          Execute
-        </v-btn>
-        <div v-if="stringifyCode" class="my-10 pa-5 rounded-lg">
-          <p
-            class="mb-0"
-            v-text="stringifyCode"
-            style="font-family: 'Fira Mono'"
-          />
+            <v-btn @click="execute" class="success text-none mt-10" large>
+              <v-icon
+                v-text="'fa-cog'"
+                size="20"
+                :class="{ rotating: loading }"
+                left
+              />
+              Execute
+            </v-btn>
+            <div v-if="stringifyCode" class="my-10 pa-5 rounded-lg">
+              <p
+                class="mb-0"
+                v-text="stringifyCode"
+                style="font-family: 'Fira Mono'"
+              />
+            </div>
+          </div>
+
+          <v-simple-table height="500">
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th
+                    v-for="header in lexicalTableHeaders"
+                    :key="header.value"
+                    class="font-weight-bold text-body-1"
+                  >
+                    <span v-text="header.text" />
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in lexicalData" :key="item.name">
+                  <td v-for="header in lexicalTableHeaders" :key="header.value">
+                    <span v-text="item[header.value]" />
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
         </div>
         <v-snackbar v-model="saved" timeout="3000" top right close
           >Saved</v-snackbar
@@ -65,7 +92,14 @@ export default {
       code: "",
       stringifyCode: "",
       loading: false,
-      saved: false
+      saved: false,
+      lexicalData: [],
+      lexicalTableHeaders: [
+        { text: "Lexeme", value: "value" },
+        { text: "Token", value: "title" },
+        { text: "Description", value: "description" },
+        { text: "Ln", value: "line" }
+      ]
     };
   },
   methods: {
@@ -76,16 +110,26 @@ export default {
       localStorage.setItem("code", this.code);
       this.saved = true;
     },
-    execute() {
-      console.log(this.code);
+    async execute() {
+      const raw_data = JSON.stringify([
+        ...this.code.replaceAll("\n", " \n").split("\n")
+      ]);
 
       this.loading = true;
-      setTimeout(() => {
-        this.stringifyCode = JSON.stringify([
-          ...this.code.replaceAll("\n", " \n").split("\n")
-        ]);
-        this.loading = false;
-      }, 1500);
+
+      const { data = [] } = await this.axios.post(
+        "http://localhost:8000/lexxer/execute/",
+        {
+          raw_data
+        }
+      );
+
+      console.log(data);
+
+      this.lexicalData = [...data.lexxer];
+
+      this.stringifyCode = raw_data;
+      this.loading = false;
     },
     smartTyper(event) {
       const pairs = {
@@ -134,9 +178,7 @@ export default {
   font-family: Fira Mono, Consolas, Menlo, Courier, monospace;
   font-size: 16px;
   line-height: 1.6;
-  max-height: 400px;
 }
-
 .prism-editor__textarea:focus {
   outline: none;
 }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "typeface-fira-mono";
 import "@fortawesome/fontawesome-free/css/all.css";
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
